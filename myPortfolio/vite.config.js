b78fe15ec7c41fd847e13da0f2041835341a259d
SoumyadipYT-OSS <soumyadipmajumder2003mcst1975@outlook.com>
2024-05-11 13:23:10 +0530

    updated

--- myPortfolio/vite.config.js
+++ myPortfolio/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// add the beginning of your app entry
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,8 @@ export default defineConfig({
 
   build: {
     rollupOptions: {
-      external: ['react-intersection-observer', 'framer-motion']
+      external: ['react-intersection-observer', 'framer-motion'],
+      manifest: true,
     },
   },
 });
