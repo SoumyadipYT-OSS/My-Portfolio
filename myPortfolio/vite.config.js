c9a6e2528d90ee836a03308e1dd4097af55334d1
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2024-06-01 22:44:54 +0530

    Update vite.config.js

--- myPortfolio/vite.config.js
+++ myPortfolio/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/My-Portfolio/',
   build: {
     rollupOptions: {
       external: ['react-intersection-observer', 'framer-motion'],
